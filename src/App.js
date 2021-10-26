c1c8b8fc3d296aee3b43ae278b12e6a0030fdec8
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-10-26 14:29:48 +0200

    complete bonus 1 w/o css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Counter from './components/Counter'
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      count: 0
+    }
+  }
+
+  substract = () => {
+    if (this.state.count > 0) {
+      return this.setState({count: this.state.count -= 1})
+    }
+  }
+
+  increment = () => (
+    this.setState({count: this.state.count += 1})
+  )
+  
+  render() {
+    return (
+      <div>
+        <h1>Counter</h1>
+        <Counter count={this.state.count} increment={this.increment}  substract={this.substract}/>
+      </div>
+    )
+  }
+} 
 
 export default App;
+
